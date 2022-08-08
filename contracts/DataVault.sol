119672edcd48fc1ef3b1bc136eb3c7bdc09d1bff
SamyakJain2020 <samyakjainking@gmail.com>
2022-08-08 23:43:34 +0530

    Data Vault Smart Contract V2

---
+++ contracts/DataVault.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// for Hardhat
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+//for remix
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+enum Status {
+    ACTIVE,
+    INACTIVE
+}
+//  Status = [ACTIVE, INACTIVE]
+struct ORG {
+    string ORGNAME;
+    uint256 ORGID;
+    mapping(address => Status) ORGUSERS;
+    mapping(address => Status) ORGADMINS;
+    address[] users;
+    address[] admins;
+}
+
+struct user {
+    string name;
+    address ad;
+    string AadharRef;
+}
+
+contract DataVault {
+    mapping(uint256 => ORG) oraganisations;
+    uint256[] organisationIds;
+
+    mapping(address => user) users;
+    address[] usersIds;
+
+    event OrganisationRegistered(uint256 orgid, string orgname);
+    event UserRegistered(address user, string name, string AadharRef);
+
+    modifier isAdmin(uint256 id) {
+        require(
+            msg.sender == address(this) &&
+                oraganisations[id].ORGADMINS[msg.sender] == Status.ACTIVE,
+            "User Not an Active Organisation Admin"
+        );
+        _;
+    }
+    modifier isActiveUser(uint256 id) {
+        require(
+            msg.sender == address(this) &&
+                oraganisations[id].ORGUSERS[msg.sender] == Status.ACTIVE,
+            "User Not an Active Organisation User"
+        );
+        _;
+    }
+    modifier isActive(uint256 id) {
+        require(
+            oraganisations[id].ORGUSERS[msg.sender] == Status.ACTIVE ||
+                oraganisations[id].ORGADMINS[msg.sender] == Status.ACTIVE,
+            "User Not an Active Organisation User or Admin"
+        );
+        _;
+    }
+
+    function registerOrganisation(string memory org_name)
+        public
+        returns (uint256)
+    {
+        organisationIds.push();
+        uint256 index = organisationIds.length - 1;
+        address admin = msg.sender;
+
+        oraganisations[index].ORGNAME = org_name;
+        oraganisations[index].ORGID = organisationIds.length;
+        oraganisations[index].ORGUSERS[admin] = Status.ACTIVE;
+        oraganisations[index].ORGADMINS[admin] = Status.ACTIVE;
+        oraganisations[index].users.push(admin);
+        oraganisations[index].admins.push(admin);
+        organisationIds[index] = organisationIds.length;
+
+        emit OrganisationRegistered(oraganisations[index].ORGID, org_name);
+        return oraganisations[index].ORGID;
+    }
+
+    // organistion active ORGADMIN can add to ORGUSERS to the organisation
+    function addUser(uint256 id, address user) public isAdmin(id) returns (bool) {
+            // using modifier to check if user is admin
+            oraganisations[id].ORGUSERS[user] = Status.ACTIVE;
+            oraganisations[id].users.push(user);
+            emit UserRegistered(user, users[user].name, users[user].AadharRef);
+            return true;
+        
+    }
+
+    // Register User with custom Aadhar Reference
+    function registerUser(string memory name, string memory AadharRef)
+        public
+        returns (address)
+    {
+        usersIds.push();
+        uint256 index = usersIds.length - 1;
+        address user = msg.sender;
+        users[user].name = name;
+        users[user].ad = user;
+        users[user].AadharRef = AadharRef;
+        usersIds[index] = user;
+        emit UserRegistered(user, name, AadharRef);
+        return usersIds[index];
+    }
+
+    // Register User with default Aadhar Reference as msg.sender (User Wallet Address)
+    function registerUser(string memory name) public returns (address) {
+        usersIds.push();
+        uint256 index = usersIds.length - 1;
+        address user = msg.sender;
+        users[user].name = name;
+        users[user].ad = user;
+        
+        // TODO BY OM AND SOHAM
+        users[user].AadharRef = ""; // to convert address(msg.sender) to string
+
+        usersIds[index] = user;
+        emit UserRegistered(user, name, users[user].AadharRef);
+        return usersIds[index];
+    }
+
+    // fetching user details using user address/id
+    function getUser(address a) public view returns (user memory) {
+        return users[a];
+    }
+
+    // fetching user aadharRef using user address/id
+    function getUserAadharRef(address a) public view returns (string memory) {
+        return users[a].AadharRef;
+    }
+
+    //get all orgusers of an organisation
+    function getOrgUsers(uint256 id) public view returns (address[] memory) {
+        return oraganisations[id].users;
+    }
+
+    //get all orgadmins of an organisation
+    function getOrgAdmins(uint256 id) public view returns (address[] memory) {
+        return oraganisations[id].admins;
+    }
+
+    // get all organisations in the system
+    function getAllOrganisations() public view returns (uint256[] memory) {
+        return organisationIds;
+    }
+
+    // get all users in the system
+    function getAllUsers() public view returns (address[] memory) {
+        return usersIds;
+    }
+
+    // Function IsUserRegisteredForOrganization
+
+}
---
+++ contracts/datavaultV1.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+ struct oraganisationid{
+        string name;
+        address[] ORGUSER;// active inactive
+        address ORGADMIN;
+    }
+      struct user{
+        string name;
+        string add;
+        address wallet;
+    string Aadhar_no;
+    }
+contract store{
+
+
+//     struct Org {
+// ...
+// }
+// mapping(address => Org) organisations;
+// address[] organisationIds;
+
+    uint randomid = 0;
+    mapping(string => oraganisationid) oraganisations;
+    mapping(address => user) users;
+   
+    function registerOrganisation(string memory org_name) public returns (string memory){
+        // create id using builtin function
+        
+        randomid++; 
+        // string memory id = Strings.toString(randomid);
+        // string memory id =  Strings.toString(keccak256(abi.encodePacked(block.timestamp,org_name,randomid)));
+        string memory id = string(abi.encodePacked(keccak256(abi.encodePacked(block.timestamp,org_name,randomid))));
+        oraganisations[id].name = org_name;
+        return id;//emit event
+    }
+
+    function registerParticipant(string memory oid,string memory pid) public {
+        // oraganisations[oid].participants.push(pid);//emit event
+    }
+
+    function getlsOrganisation(string memory oid) public view returns (oraganisationid memory) {
+        //get partincipants from organosations
+        return oraganisations[oid];//emit event
+    }
+
+    function getlsParticipants(string memory oid) public view returns (string[] memory){
+        // return line
+        return oraganisations[oid].participants;
+    }
+}
+
+/*
+Q) Vault kya hota hai ?, What Functionalities can be stored in the Data Vault ?
+Q) Functioning of the Data Vault ?
+Q) SSI for Proxy Authentication ?
+Q) Is this a Digi Locker Type thing ?
+Q) Create a Source of Truth for the Data Vault -> Blockchian solution
+Q) How to add data in the Data Vault in the Blockchain ? -> By consumer Agency
+Q) How to check Validity of the Data in the Data Vault in the Blockchain -> No validation
+Q) Which Blockcahin -> public/Private ->
+Q) Prototype of the Data Vault ->
+Q) OpenZeppelin Access Modifier -> For Access Control of the vault -> org Access Control given to ORGADMIN and ORGUSER(with access to the vault)
+Q) Retriving access control of the vault ->
+
+
+
+APIs:
+1. SSI APIs for Rwact
+2. Auth API for Server Side Authentication -> Sign a message in React 
+                                           -> SuperAccess (Gov), Partisipant Access (OrgAdmin)(Data Add, Data Update)
+3. We can Provide this a B(Gov)2B(Gov) Api based Service
+4. We can Provide this a B(Gov)2B(User) Frontend based Txn
+5. It can be implemented in any Platform
+
+
+SIH:
+1. Bisness Logic ,costing and scale
+2. UI -> React -> USE A ADMIN THME
+3. Do both the things (B2B + B2C)
+4. 1st PITCH -> Ideation + Bussiness 8 min pitch -> Mockups , NonTech Stuff , Prod Functionalities -> Business Case -> 60% prod 40% TEch
+5. Final Pitch -> To present it to all the Evaluators (PRE-Prepare Slides) <Blockchain , PUblic Usecase>
+6. Development Eval -> Postman Output , Frontend UI Demo , <Visual IMpact> , Put Dummy Data in the BlockChain for Best USe case Senario + Partical Functioning demo
+7. Final Presentation -> Presentation + Full Demo
+
+8. IDEA SHOULD BE CONCRETE + 1 EXPERT
+9. DONT SHOW -VE STUFF BUT -----> Cost Analysis -> 1 unit = ? matic -> Gas Analisis -> OR Private Blockcahin Running Cost
+10. Split the Work -> Pass the Question -> Keep Each Others back
+*/
+
